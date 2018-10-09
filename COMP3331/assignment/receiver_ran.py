0bcc704e12d927967510a8774580618cd49fce57
Yyyyyyyao <auyy1997@gmail.com>
2018-10-10 01:12:52 +1100

    Update receiver_ran.py

--- COMP3331/assignment/receiver_ran.py
+++ COMP3331/assignment/receiver_ran.py
@@ -20,8 +20,19 @@ def main():
 	global total_data
 	global total_seg
 	global total_data_seg
-	global total_Dup_data
+	global total_error
+	global total_dup_receive
 	global total_Dup_ACKs_sent
+
+
+	total_data = 0
+	total_seg = 0
+	total_data_seg = 0
+	total_error = 0
+	total_dup_receive = 0
+	total_Dup_ACKs_sent = 0
+
+
 
 	first_pkt = False
 
@@ -38,6 +49,7 @@ def main():
 	rcv_buffer = []
 
 	dup_ack = 0
+	ini_rcv_seq = 0
 
 	receiverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	receiverSocket.bind(("localhost", receiver_port))
@@ -58,6 +70,10 @@ def main():
 	    if (state == STATE_END):
 			curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
 			log_data = "rcv\t"+str(curr_time)+"\tA\t"+str(get_seq(pkt_receive))+'\t'+str(len(get_data(pkt_receive)))+'\t'+str(get_ack(pkt_receive))+'\n'
+			total_seg = total_seg + 1
+			total_data_seg = total_seg - 4
+
+			print(total_dup_receive)
 			receiver_log.write(log_data)
 			sys.exit()
 
@@ -66,7 +82,11 @@ def main():
 	    	state = STATE_END
 	    	curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
 	    	log_data = "rcv\t"+str(curr_time)+"\tF\t"+str(get_seq(pkt_receive))+'\t'+ str(len(get_data(pkt_receive)))+'\t'+str(get_ack(pkt_receive))+'\n'
+	    	total_seg = total_seg + 1
 	    	receiver_log.write(log_data)
+
+
+	    	total_data = int(get_ack(pkt_receive)) - ini_rcv_seq
 
 	    	ack = get_seq(pkt_receive)+1
 	    	seq = get_ack(pkt_receive)
@@ -97,6 +117,7 @@ def main():
 	    		curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
 	    		
 	    		log_data = "rcv\t"+str(curr_time)+"\tS\t"+str(get_seq(pkt_receive))+'\t'+ str(len(get_data(pkt_receive)))+'\t'+str(get_ack(pkt_receive))+'\n'
+	    		total_seg = total_seg + 1
 	    		receiver_log.write(log_data)
 
 	    		ack = int(get_seq(pkt_receive))+1
@@ -112,6 +133,8 @@ def main():
 
 	    		cumulated_ack = ack
 
+	    		ini_rcv_seq = ack
+
 	    		log_data = "snd\t"+str(curr_time)+"\tSA\t"+str(seq)+'\t'+ str(len(get_data(pkt_1)))+'\t'+str(get_ack(pkt_1))+'\n'
 	    		receiver_log.write(log_data)
 	    
@@ -121,52 +144,73 @@ def main():
 	    	data = str(get_data(pkt_receive))
 	    	checksum_header = get_checksum(pkt_receive)
 	    	checksum_data = calculate_checksum(data)
-	    	if(checksum_data == checksum_header):
+	    	dup_sent = False
+
+
+	    	if(checksum_data != checksum_header):
+	    		total_error = total_error + 1
+	    	else:
 		    	if(is_ack(pkt_receive)):
 		    		curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
 		    		log_data = "rcv\t"+str(curr_time)+"\tA\t"+str(get_seq(pkt_receive))+'\t'+str(len(get_data(pkt_receive)))+'\t'+str(get_ack(pkt_receive))+'\n'
+		    		total_seg = total_seg + 1
 		    		receiver_log.write(log_data)
 		    	else:
 
 		    		curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
 		    		log_data = "rcv\t"+str(curr_time)+"\tD\t"+str(get_seq(pkt_receive))+'\t'+str(len(get_data(pkt_receive)))+'\t'+str(get_ack(pkt_receive))+'\n'
+		    		total_seg = total_seg + 1
 		    		receiver_log.write(log_data)
+
 
 		    		if(seq_receive == seq and not first_pkt):
 
 		    			first_pkt = True
 		    			cumulated_ack = seq_receive+data_length
 		    			write_to_file(file_name, str(get_data(pkt_receive)))
+		    			pre_seq = int(get_seq(pkt_receive))
 		    			
-		    		if (cumulated_ack == seq_receive):
+		    		elif (cumulated_ack == seq_receive and first_pkt):
 
-		    			if(not rcv_buffer):
-		    				cumulated_ack = seq_receive+data_length
-		    				write_to_file(file_name, str(get_data(pkt_receive)))
+		    			if(pre_seq == seq_receive and pre_seq >= cumulated_ack):
+		    				total_dup_receive = total_dup_receive + 1
+		    				dup_sent = True
 		    			else:
-		    				write_to_file(file_name, str(get_data(pkt_receive)))
-		    				cumulated_ack = seq_receive+data_length
-		    				for z in rcv_buffer:
-		    					if (int(cumulated_ack) ==int(get_seq(z))):
-		    						write_to_file(file_name, str(get_data(z)))
-		    						cumulated_ack = int(get_seq(z))+int(len(get_data(z)))
-		    				rcv_buffer[:] = [value for value in rcv_buffer if int(get_seq(value)) > int(cumulated_ack)]
+		    				pre_seq = seq_receive
+			    			if(not rcv_buffer):
+			    				cumulated_ack = seq_receive+data_length
+			    				write_to_file(file_name, str(get_data(pkt_receive)))
+			    			else:
+			    				write_to_file(file_name, str(get_data(pkt_receive)))
+			    				cumulated_ack = seq_receive+data_length
+			    				for z in rcv_buffer:
+			    					if (int(cumulated_ack) ==int(get_seq(z))):
+			    						write_to_file(file_name, str(get_data(z)))
+			    						cumulated_ack = int(get_seq(z))+int(len(get_data(z)))
+			    				rcv_buffer[:] = [value for value in rcv_buffer if int(get_seq(value)) > int(cumulated_ack)]
 
 		    		else:
-		    			rcv_buffer.append(pkt_receive)
+		    			if(pre_seq == seq_receive and pre_seq >= cumulated_ack):
+		    				total_dup_receive = total_dup_receive + 1
+		    				dup_sent = True
+		    			else:
+		    				pre_seq = seq_receive
+		    				rcv_buffer.append(pkt_receive)
 
-		    		flag = 0b010
-		    		pkt = packet(1, cumulated_ack, '', flag, -2)
-		    		receiverSocket.sendto(str(pkt), addr)
-		    		if(dup_ack == int(get_ack(pkt))):
-		    			curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
-		    			log_data = "snd/DA\t"+str(curr_time)+"\tA\t"+str(get_seq(pkt))+'\t'+str(len(get_data(pkt)))+'\t'+str(get_ack(pkt))+'\n'
-		    			receiver_log.write(log_data)
-		    		else:
-		    			dup_ack = int(get_ack(pkt))
-		    			curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
-		    			log_data = "snd\t"+str(curr_time)+"\tA\t"+str(get_seq(pkt))+'\t'+str(len(get_data(pkt)))+'\t'+str(get_ack(pkt))+'\n'
-		    			receiver_log.write(log_data)
+		    		if(not dup_sent):
+			    		flag = 0b010
+			    		pkt = packet(1, cumulated_ack, '', flag, -2)
+			    		receiverSocket.sendto(str(pkt), addr)
+			    		if(dup_ack == int(get_ack(pkt))):
+			    			curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
+			    			log_data = "snd/DA\t"+str(curr_time)+"\tA\t"+str(get_seq(pkt))+'\t'+str(len(get_data(pkt)))+'\t'+str(get_ack(pkt))+'\n'
+			    			total_Dup_ACKs_sent = total_Dup_ACKs_sent + 1
+			    			receiver_log.write(log_data)
+			    		else:
+			    			dup_ack = int(get_ack(pkt))
+			    			curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
+			    			log_data = "snd\t"+str(curr_time)+"\tA\t"+str(get_seq(pkt))+'\t'+str(len(get_data(pkt)))+'\t'+str(get_ack(pkt))+'\n'
+			    			receiver_log.write(log_data)
 		    		#curr_time = float("{:6.2f}".format(time.time()*1000 - start_time))
 		    		#log_data = "snd\t"+str(curr_time)+"\tA\t"+str(get_seq(pkt))+'\t'+str(len(get_data(pkt)))+'\t'+str(get_ack(pkt))+'\n'
 		    		#receiver_log.write(log_data)
@@ -231,6 +275,12 @@ def main():
 						receiver_log.write(log_data)
 
 			'''
+
+
+
+
+
+
 def write_to_file(file_name, data):
 	with open(file_name, "ab") as file:
 		file.write(data)
